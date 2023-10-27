01e49d97ae865993592220f7470e5844d6542923
Inna <prizrak1609@gmail.com>
2023-10-27 15:27:40 +0300

    Add project files.

---
+++ Play.cs
@@ -0,0 +1,35 @@
+﻿//Завдання 1: Створіть клас «П'єса», який має зберігати таку інформацію:
+// назва п'єси;  П.І.Б.автора;  жанр;  рік.
+//Реалізуйте у класі методи та властивості, необхідні для функціонування класу.
+//Додайте до класу деструктор.
+//Напишіть код для тестування функціональності класу.
+//Напишіть код для деструктора.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Homework_7_1
+{
+    internal class Play
+    {
+        public string NamePlay;
+        public string FullNameAuthor;
+        public string Genre;
+        public string Year;
+        public Play(string N, string A, string G, string Y)
+        {
+            NamePlay = N;
+            FullNameAuthor = A;
+            Genre = G;
+            Year = Y;
+        }
+        ~Play()
+        {
+            Console.WriteLine("Відпрацювання фіналізатора.");
+            Thread.Sleep(60000);//Затримка часу спрацювання фіналізатора
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿//Завдання 1: Створіть клас «П'єса», який має зберігати таку інформацію:
+// назва п'єси;  П.І.Б.автора;  жанр;  рік.
+//Реалізуйте у класі методи та властивості, необхідні для функціонування класу.
+//Додайте до класу деструктор.
+//Напишіть код для тестування функціональності класу.
+//Напишіть код для деструктора.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_7_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Play play = new Play("Назва п'єси", "Автор п'єси", "Жанр п'єси", "Рік написання п'єси");
+            Console.WriteLine($"П'єса: {play.NamePlay} {play.FullNameAuthor} {play.Genre} {play.Year}");
+            Console.ReadLine();
+        }            
+    }
+}
